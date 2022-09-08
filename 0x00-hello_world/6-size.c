f9a8d64f278cac590adbeb985e3e9562d37f6ea2
Emediong Benison <emediongbenison@yahoo.com>
2022-09-08 07:50:55 -0700

    push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,5 @@ int main(void)
 	printf("Size of an int: %i byte(s)\n", sizeof((integer));
 	printf("Size of long: %d byte(s)\n", sizeof((isLong));
 	printf("Size of long long: %Lf byte(s)\n", sizeof((twiceLong));
-	printf("Size of float: %f byte(s)\n", sizeof((iamFloat);
+	printf("Size of float: %f byte(s)\n", sizeof((iamFloat));
 }
