2758e948c9b90a44acaf8e14a188cfe6fc57019f
Emediong Benison <emediongbenison@yahoo.com>
2022-09-09 10:47:53 -0700

    fixed betty linting

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,19 +3,17 @@
 #include <stdio.h>
 
 /**
- * main - prints the last digit of a random 
- * numbered stored in n.
- * Return: 0 Always Success
-*/
-
+  * main - prints the last digit of a random.
+  * number stored in n
+  * Return: 0 Always Success
+  */
 int main(void)
-{	
+{
 	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
-
 	if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	else if (last_digit == 0)
