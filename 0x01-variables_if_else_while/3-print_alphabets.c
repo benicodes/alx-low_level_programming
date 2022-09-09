0d2dd3061fd365e5d3fdc00cddbe5476409794b3
Emediong Benison <emediongbenison@yahoo.com>
2022-09-09 11:12:46 -0700

    task 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - entry file
+  * description: prints alphabet in lower and upper case
+  * Return: 0 Always success
+  */
+
+int main(void)
+{
+	char l, u;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	for (u = 'A'; u <= 'Z'; u++)
+		putchar(u);
+	putchar('\n);
+	return (0);
+}
