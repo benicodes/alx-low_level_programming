5a0549d8b1d51b2511ef21826ffd12b0783cf257
Emediong Benison <emediongbenison@yahoo.com>
2022-09-09 08:12:20 -0700

    Task 0 commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,23 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
-
-/** 
- * main - entry file
- *
- * description: a program to retun n number using if else 
- *
- * retun (0) successful 
- */
-
+/**
+  * main - entry file
+  * description: checks the number and print it on the screen
+  * Return: 0 Always success
+  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if(n > 0)
-		printf("is positive\n");
+	if (n > 0)
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("is zero\n");
-	else
-		printf("is negative\n");
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	return (0);
 }
