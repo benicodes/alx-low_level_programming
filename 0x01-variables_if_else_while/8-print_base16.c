4a9f1d97b40ccdd173f11c7872da303a434dcabe
Emediong Benison <emediongbenison@yahoo.com>
2022-09-11 12:41:45 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry file
+ * description: print numbers in hex
+ * Return: 0 Always success
+ */
+
+int main(void)
+{
+	int i;
+	char k;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+
+	for (k = 'a'; k <= f; k++)
+		putchar(k);
+
+	putchar('\n');
+	return (0);
+}
