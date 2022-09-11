7801a0c288df041fa4a983ccd34bc803f541a981
Emediong Benison <emediongbenison@yahoo.com>
2022-09-11 12:17:31 -0700

    task 5 betty fixed

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,18 +2,18 @@
 
 /**
  * main - entry file
- * description: program to print all single digit 
- * numbers of base 10 starting from 0
- * Return: 0 Always success
+ * description - function to print numbers
+ * then end with a new line
+ * Return (0) Success
  */
 
 int main(void)
 {
 	int n;
+
 	for (n = 0; n < 10; n++)
 		printf("%d", n);
 
 	putchar('\n');
 	return (0);
-
 }
