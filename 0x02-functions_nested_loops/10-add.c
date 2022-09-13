e3c4423a68042ef10b549a193bddcd3eeb8ef3b7
Emediong Benison <emediongbenison@yahoo.com>
2022-09-13 15:18:35 -0700

    Task 10 hot fix

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers and gets the result
+ * @num1: number to be added to num2
+ * @num2: number to be added to num1
+ * Return: result of num1 plus num2
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
