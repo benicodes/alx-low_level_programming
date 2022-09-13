ca2d4040ebae0e1165e84bc77528c12b8056bcf7
Emediong Benison <emediongbenison@yahoo.com>
2022-09-13 13:37:07 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - function checks for lowercase characters
+ * @i: the character to check
+ * Return: 1 (success), 0 (fail)
+ */
+
+int _islower(int i)
+{
+	return (i >= 'a' && i <= 'z');
+}
+
