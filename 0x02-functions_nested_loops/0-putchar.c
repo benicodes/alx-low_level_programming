ae6c51da791a158c445a916650bc3446662a6c8c
Emediong Benison <emediongbenison@yahoo.com>
2022-09-13 02:11:10 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry file
+ * description: this propram prints _putchar, followed by a new line
+ * Return: 0 Always Success
+ */
+
+int main(void)
+{
+	putchar(_putchar, "\n");
+	return (0);
+}
