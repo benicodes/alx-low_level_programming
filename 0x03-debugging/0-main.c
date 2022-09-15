c567be291d10f5575e46f8d574aa2ef37cae3250
Emediong Benison <emediongbenison@yahoo.com>
2022-09-14 19:26:06 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include "main.h"
 
 /**
@@ -13,13 +10,8 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	n = 0;
+
+	positive_or_negative(n);
 	return (0);
 }
