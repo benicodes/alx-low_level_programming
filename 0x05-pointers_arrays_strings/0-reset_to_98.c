17414dd8dba5d352c3a7e41bd6e00166c90fd1eb
Emediong Benison <emediongbenison@yahoo.com>
2022-09-19 17:45:42 -0700

    Pointers, arrays and strings tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - will update the value of variable
+ * pointer points to 98
+ * @n: pointer to the variable to update
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+# Introduction to pointers in C programming
+# Arrays in C programming and 
+# Strings in C
+
