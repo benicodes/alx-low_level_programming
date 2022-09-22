11b9f834be1b608b4a5ff7fd500dd478845197a8
Emediong Benison <emediongbenison@yahoo.com>
2022-09-22 07:35:56 -0700

    Final Advance Task - Buffer

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * infinte_add - add two numbers
+ * @n1: char 1
+ * @n2: char 2
+ * @r: char 3
+ * @size_r: int
+ * Return: char
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int a, b, c, d, e, f;
+	
+	for (a = 0; n1(a); a++)
+	{
+
+	}
+}
