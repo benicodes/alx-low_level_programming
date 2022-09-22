c8c4c2f90e632bb9fe1bae8d192288346033db40
Emediong Benison <emediongbenison@yahoo.com>
2022-09-22 04:18:11 -0700

    capitalizes all words of a string - Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: the modified string
+ * Return: result of modified string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char word[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == word[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
+			}
+		}
+	}
+}
