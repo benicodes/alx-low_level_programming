a7f4b07b93d9adab6b03eb218651fa04096da5b0
Emediong Benison <emediongbenison@yahoo.com>
2022-09-27 06:10:13 -0700

    Advanced task 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a poiter to a char
+ * @s: pointer to pointer set
+ * @to: string set
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
