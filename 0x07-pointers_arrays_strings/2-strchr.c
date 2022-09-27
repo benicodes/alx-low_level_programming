9b95ef2578cbf165a05329fdebcc83f95e0e47ea
Emediong Benison <emediongbenison@yahoo.com>
2022-09-27 01:27:18 -0700

    Task 2 - String character memory locator function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,10 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != c && *s != "\0")
+	while (*s != c && *s != '\0')
 		s++;
 	if (*s == c)
 		return (s);
 	else
-		return ("\0");
+		return ('\0');
 }
