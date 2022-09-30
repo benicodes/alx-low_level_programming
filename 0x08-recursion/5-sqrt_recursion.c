12ad0fc7db88c13d7c4bb4a52473fbf81510f991
Emediong Benison <emediongbenison@yahoo.com>
2022-09-29 18:01:59 -0700

    Task 5 quick fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,7 @@ int actual_sqrt_recursion(int n, int i);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of 
+ * @n: number to calculate the square root of
  * Return: the resulting square root
  */
 
@@ -19,6 +19,7 @@ int _sqrt_recursion(int n)
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a given number
  * @i: iterable variable
+ * @n: number to calculate the square root
  * Return: the recursion result
  */
 
